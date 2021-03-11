523d29fc95886616eb7c6be00144b5f15bac4751
Ivan Egorov <www.Len244@gmail.com>
2021-03-11 15:39:36 +0300

    Implemented simpliest thread solution

--- word_counter/CounterInDll/CounterProcessor.cs
+++ word_counter/CounterInDll/CounterProcessor.cs
@@ -4,12 +4,14 @@ using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
+using System.Diagnostics;
+using System;
+using System.Threading;
 
 namespace CounterInDll
 {
     public class CounterProcessor
     {
-
         private XmlNamespaceManager namespaceManager;
         private Regex regex = new Regex(@"[^ ,.\/\\><:;\-0-9\[\]\(\)=" + "?\"!{ }]+");
         private Dictionary<string, int> wordsFrequency = new Dictionary<string, int>();
@@ -22,6 +24,11 @@ namespace CounterInDll
 
         private Dictionary<string, int> computeWords(XDocument document)
         {
+
+            Stopwatch oneThreadStopwatch = new Stopwatch();
+            Console.WriteLine("Method with one thread started");
+            oneThreadStopwatch.Start();
+
             var body = document.Root.XPathSelectElements("fb:body/fb:section/fb:p", namespaceManager).ToList();
 
             body.ForEach(delegate (XElement name)
@@ -45,7 +52,80 @@ namespace CounterInDll
 
             var sortedWordsFrequency = wordsFrequency.OrderBy(x => x.Value).Reverse().ToDictionary(x => x.Key, x => x.Value);
 
+            oneThreadStopwatch.Stop();
+            Console.WriteLine($"Method with one thread ended with time: {oneThreadStopwatch.Elapsed}");
+
             return sortedWordsFrequency;
+        } 
+        
+        public Dictionary<string, int> MultithrededComputeWords(XDocument document)
+        {
+
+            Stopwatch multiThreadStopwatch = new Stopwatch();
+            Console.WriteLine("Method with multi threads started");
+            multiThreadStopwatch.Start();
+
+            var body = document.Root.XPathSelectElements("fb:body/fb:section/fb:p", namespaceManager).ToList();
+
+            var secondThreadEnd = body.Count();
+            var firstThreadStart = 0;
+            Console.WriteLine(secondThreadEnd + "");
+            var firtsThreadEnd = (int)Math.Round((Double)secondThreadEnd / 2);
+            Console.WriteLine(firtsThreadEnd + "");
+            var secondThreadStart = firtsThreadEnd + 1;
+
+            var firstHalfThread = new Thread(() => SplitParsingAndWriting(firstThreadStart, firtsThreadEnd, body));
+            firstHalfThread.Start();
+            var secondHalfThread = new Thread(() => SplitParsingAndWriting(secondThreadStart, secondThreadEnd, body));
+            secondHalfThread.Start();
+
+            body.ForEach(delegate (XElement name)
+            {
+                MatchCollection matches = regex.Matches(name.Value);
+
+                foreach (Match match in matches)
+                {
+                    var matchAsString = match.ToString().ToLower();
+
+                    if (wordsFrequency.ContainsKey(matchAsString))
+                    {
+                        wordsFrequency[matchAsString] += 1;
+                    }
+                    else
+                    {
+                        wordsFrequency[matchAsString] = 1;
+                    }
+                }
+            });
+
+            var sortedWordsFrequency = wordsFrequency.OrderBy(x => x.Value).Reverse().ToDictionary(x => x.Key, x => x.Value);
+
+            multiThreadStopwatch.Stop();
+            Console.WriteLine($"Method with multi threads ended with time: {multiThreadStopwatch.Elapsed}");
+
+            return sortedWordsFrequency;
+        }
+
+        private void SplitParsingAndWriting(int threadStart, int threadEnd, List<XElement> body)
+        {
+            for (int i = threadStart; i < threadEnd; i++)
+            {
+                MatchCollection matches = regex.Matches(body[i].Value);
+
+                foreach (Match match in matches)
+                {
+                    var matchAsString = match.ToString().ToLower();
+
+                    if (wordsFrequency.ContainsKey(matchAsString))
+                    {
+                        wordsFrequency[matchAsString] += 1;
+                    }
+                    else
+                    {
+                        wordsFrequency[matchAsString] = 1;
+                    }
+                }
+            }
         }
     }
 
--- word_counter/Main/Program.cs
+++ word_counter/Main/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using CounterInDll;
+using System.Diagnostics;
 
 namespace Main
 {
@@ -11,14 +12,36 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            XDocument doc = XDocument.Load(@"P:\TaskFiles\tolstoj_lew_nikolaewich-text_0040.fb2");
+            Stopwatch globalStopwatch = new Stopwatch();
+            Console.WriteLine("Global Timer Started");
+            globalStopwatch.Start();
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            XDocument doc = XDocument.Load(@$"{path}\tolstoj_lew_nikolaewich-text_0040.fb2");
 
             CounterProcessor counterProcessor = (CounterProcessor)typeof(CounterProcessor)
                 .Assembly.CreateInstance("CounterInDll.CounterProcessor");
 
+            //Stopwatch oneThreadStopwatch = new Stopwatch();
+            //Console.WriteLine("Method with one thread started");
+            //oneThreadStopwatch.Start();
+
             Dictionary<String, int> sortedWordsFrequency = (Dictionary<string, int>)counterProcessor.GetType()
                 .GetMethod("computeWords", BindingFlags.NonPublic | BindingFlags.Instance)
                 .Invoke(counterProcessor,new object[] { doc });
+
+            //oneThreadStopwatch.Stop();
+            //Console.WriteLine($"Method with one thread ended with time: {oneThreadStopwatch.Elapsed}");
+
+
+            //Stopwatch multiThreadStopwatch = new Stopwatch();
+            //Console.WriteLine("Method with multi threads started");
+            //multiThreadStopwatch.Start();
+
+            Dictionary<String, int> publicMultiThreaded = counterProcessor.MultithrededComputeWords(doc);
+
+            //multiThreadStopwatch.Stop();
+            //Console.WriteLine($"Method with multi threads ended with time: {multiThreadStopwatch.Elapsed}");
 
             using (StreamWriter outputFile = new StreamWriter(@"words_frequency.txt"))
             {
@@ -27,6 +50,9 @@ namespace Main
                     outputFile.WriteLine($"{item.Key} : {item.Value}");
                 }
             }
+
+            globalStopwatch.Stop();
+            Console.WriteLine($"Global timer ended with time: {globalStopwatch.Elapsed}");
         }
     }
 }
